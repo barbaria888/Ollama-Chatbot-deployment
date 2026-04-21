1de877bd218381d0e9951d8ff47dc74dfed90c3a
HARDIK ARORA <arorahardik0811@gmail.com>
2026-04-21 12:20:15 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,22 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket
 import requests
+import json
+import asyncio
 
 app = FastAPI()
 
 OLLAMA_URL = "http://ollama-service:11434/api/generate"
 
-@app.get("/")
-def health():
-    return {"status": "running"}
-    
-@app.post("/chat")
-def chat(prompt: str):
-    response = requests.post(OLLAMA_URL, json={
-        "model": "tinyllama",
-        "prompt": prompt,
-        "stream": False
-    })
+@app.websocket("/ws/chat")
+async def chat(websocket: WebSocket):
+    await websocket.accept()
 
-    data = response.json()
-    return {"answer": data["response"]}
+    while True:
+        prompt = await websocket.receive_text()
 
+        response = requests.post(
+            OLLAMA_URL,
+            json={
+                "model": "tinyllama",
+                "prompt": prompt,
+                "stream": True
+            },
+            stream=True
+        )
+
+        buffer = ""
+
+        for line in response.iter_lines():
+            if line:
+                data = json.loads(line.decode("utf-8"))
+
+                token = data.get("response", "")
+                buffer += token
+
+                # Send in chunks instead of every token
+                if len(buffer) >= 3:
+                    await websocket.send_text(buffer)
+                    await asyncio.sleep(0.02)  
+                    buffer = ""
+
+        # flush remaining text
+        if buffer:
+            await websocket.send_text(buffer)
+
+        await websocket.send_text("[DONE]")
